ec252e58aaac5297b83d347a93e954b3000f42aa
Aina Mayet <aina.mayet@gmail.com>
2014-11-28 11:09:34 +0100

    Adding quick comments for handlebars helpers

--- assets/desktop/js/handlebars-modules.js
+++ assets/desktop/js/handlebars-modules.js
@@ -1,9 +1,19 @@
+/**
+ * Dynamic partial
+ * Allow us to pass a variable for the name of the partial to load
+ * exemple : {{{dynPartial logo}}}
+ */
 Handlebars.registerHelper('dynPartial', function (data, options) {
     var change = '{{> ' + data + '}}';
     var template = Handlebars.compile(change);
     return template(options.data);
 });
 
+/**
+ * Create the HTML for QTEs and calculate where
+ * they shoud be positionnated on timeline
+ * exemple : {{addingQTE}}
+ */
 Handlebars.registerHelper('addingQTE', function () {
     var qtes = this.qte,
         duration = Handlebars.escapeExpression(this.videoDuration);
@@ -17,7 +27,10 @@ Handlebars.registerHelper('addingQTE', function () {
 });
 
 
-
+/**
+ * Calculate the value of 100 / number
+ * exemple : style="width: {{percent ../sequences.length}}%"
+ */
 Handlebars.registerHelper('percent', function (nb) {
     return new Handlebars.SafeString(100/nb);
 });
